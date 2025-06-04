a027b55a74d3c54687b24311946b5a64af6f751a
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-06-04 16:03:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ gsap.from('[animate] .word', {
     scrub: true
   }
 })
+
+  gsap.registerPlugin(SplitText);
+
+document.fonts.ready.then(() => {
+  gsap.set(".container", { opacity: 1 });
+  let split = SplitText.create(".animate-me", { type: "words", aria: "hidden" });
+
+  gsap.from(split.words, {
+    opacity: 0,
+    duration: 2,
+    ease: "sine.out",
+    stagger: 0.1,
+  });
+});
